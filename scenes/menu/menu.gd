bac2bfdab025f915977444f2ab68e9ba540ddf29
Mauro Castro <maurocastruli@gmail.com>
2025-10-20 09:41:49 -0300

    escena creditos

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 var version = ProjectSettings.get_setting("application/config/version")
 @onready var version_label: Label = $Label
 var mainLevel:PackedScene = preload("res://scenes/main_scene/main_scene.tscn")
-
+const creditos := "res://scenes/creditos/creditos.tscn"
 func _ready() -> void:
 	version_label.text = "Verison: " + str(version)
 	AudioManager.Musica.play()
@@ -13,3 +13,7 @@ func _on_play_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file(creditos)
